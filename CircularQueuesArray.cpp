eab7313728102a78b962120788ff25100e1b4cba
AfdanAiman <afdanaiman06@gmail.com>
2025-06-12 11:13:07 +0700

    menambahkan pengecekan jika antrian kosong pada fungsi insert

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -25,6 +25,26 @@ public:
             cout << "\nQueue overflow\n"; //1.a
              return;
         }
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT =0;//2.a
+            REAR = 0;//2.b
+        }
+        else
+        {
+            //Jika REAR berada di posisi terakhir array,kembali ke awal array
+            if (REAR == MAX - 1)
+            {
+                REAR = 0;
+            }
+            else
+            {
+                REAR = REAR + 1;
+            }
+            queue_array[REAR] = num; 
+        }
+        
         
     }
 };
